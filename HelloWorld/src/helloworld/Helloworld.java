ada509174e3565a4228bc584d03342c456f3db8f
jjjoseph21 <jj_joseph21@hotmail.com>
2014-01-31 01:48:30 -0500

    Change to make make commit

--- HelloWorld/src/helloworld/Helloworld.java
+++ HelloWorld/src/helloworld/Helloworld.java
@@ -3,8 +3,14 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
+		
+		String state = "folly";
         
 		System.out.println("Hello World");
+		System.out.println("Just need to know I can use Git");
+		System.out.println("I guess this is not a "+state);
+		
+		
 
 	}
 
